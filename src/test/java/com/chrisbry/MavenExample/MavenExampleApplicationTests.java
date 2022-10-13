5e306bae2d80fd196c1cbff0a8e772900312a0e6
Chrisbry <c.bryantcf@gmail.com>
2022-10-13 15:36:46 -0400

    Maven Jenkins Example

---
+++ src/test/java/com/chrisbry/MavenExample/MavenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisbry.MavenExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
